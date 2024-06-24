de035d1b16810d66e6abe9e012cb4f00de72de62
yunyala <892722202@qq.com>
2024-06-24 23:42:13 +0800

    first commit

---
+++ src/main/java/com/yunya/javainterviewstudy/JavaInterviewStudyApplication.java
@@ -0,0 +1,13 @@
+package com.yunya.javainterviewstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaInterviewStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaInterviewStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yunya/javainterviewstudy/JavaInterviewStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunya.javainterviewstudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaInterviewStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
