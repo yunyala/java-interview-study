63225ca446ccac1661023c9e4d0e099a8cf6f414
yunyala <892722202@qq.com>
2024-07-17 14:09:45 +0800

    Object wait方法的使用demo 完善

--- oop/src/main/java/com/yunya/oop/demo/WaitDemo.java
+++ oop/src/main/java/com/yunya/oop/demo/WaitDemo.java
@@ -2,6 +2,16 @@ package com.yunya.oop.demo;
 
 /**
  * 线程等待demo
+ *
+ * public final void wait() throws InterruptedException：调用该方法会导致当前线程等待，直到另一个线程调用此对象的notify()方法或notifyAll()方法。
+ *
+ * ②、public final native void notify()：唤醒在此对象监视器上等待的单个线程。如果有多个线程等待，选择一个线程被唤醒。
+ *
+ * ③、public final native void notifyAll()：唤醒在此对象监视器上等待的所有线程。
+ *
+ * ④、public final native void wait(long timeout) throws InterruptedException：等待 timeout 毫秒，如果在 timeout 毫秒内没有被唤醒，会自动唤醒。
+ *
+ * ⑥、public final void wait(long timeout, int nanos) throws InterruptedException：更加精确了，等待 timeout 毫秒和 nanos 纳秒，如果在 timeout 毫秒和 nanos 纳秒内没有被唤醒，会自动唤醒。
  */
 public class WaitDemo {
     public static void main(String[] args) {
