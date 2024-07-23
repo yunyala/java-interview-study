8224b3974558ef9fde159f908657857cadc04581
yunyala <892722202@qq.com>
2024-07-23 16:45:12 +0800

    springboot 依赖注入完善注释

--- spring-di/src/main/java/com/yunya/springdi/SpringDiApplication.java
+++ spring-di/src/main/java/com/yunya/springdi/SpringDiApplication.java
@@ -1,7 +1,5 @@
 package com.yunya.springdi;
 
-//import com.yunya.springdi.constructor.UserService;
-import com.yunya.springdi.field.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
