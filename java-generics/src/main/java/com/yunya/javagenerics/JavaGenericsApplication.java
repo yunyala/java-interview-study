9d8bc96ce17966c9ddd1666920eaa89b504b521e
yunyala <892722202@qq.com>
2024-08-08 11:01:45 +0800

    Java泛型

---
+++ java-generics/src/main/java/com/yunya/javagenerics/JavaGenericsApplication.java
@@ -0,0 +1,13 @@
+package com.yunya.javagenerics;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaGenericsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaGenericsApplication.class, args);
+    }
+
+}
---
+++ java-generics/src/test/java/com/yunya/javagenerics/JavaGenericsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunya.javagenerics;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaGenericsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
